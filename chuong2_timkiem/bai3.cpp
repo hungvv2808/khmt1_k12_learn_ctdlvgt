005236210d1edfc7876399b8632b17e250e483c7
kingkong2899 <vuhung2881999@gmail.com>
2019-01-13 13:32:51 +0700

    first commit

--- chuong2_timkiem/bai3.cpp
+++ chuong2_timkiem/bai3.cpp
@@ -1,6 +1,38 @@
+#include <graphics.h>
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
 using namespace std;
-int main(){
-    cout << "helo my friend." << endl;
+int main()
+{
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, NULL);
+    setbkcolor(1); // set backgroud
+    int x = 250;
+    setcolor(12); //outline heart
+    circle(x + 50, 50, 40);
+    delay(1000);
+    circle(x + 110, 50, 40);
+    delay(500);
+    line(x + 22, 80, x + 80, 140);
+    delay(500);
+    line(x + 80, 140, x + 138, 80);
+    delay(500);
+
+    floodfill(x + 50, 50, 12); //fill heart
+    delay(500);
+    floodfill(x + 110, 50, 12);
+    delay(500);
+    floodfill(x + 80, 50, 12);
+    delay(500);
+    floodfill(x + 80, 100, 12);
+    delay(500);
+
+    setcolor(RED);
+    outtextxy(x + 35, 50, "I L O V E U");
+    outtextxy(x + 35, 150, "K I N G K O N G");
+
+    cin.get();
+    closegraph();
     return 0;
 }
